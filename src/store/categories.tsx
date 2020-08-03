b6790c5f76821ff0100370215f7088a7fb29614d
TadasBalsys <balsys.tadas@gmail.com>
2020-08-03 14:22:03 +0300

    Changed structure in categories object

--- src/store/categories.tsx
+++ src/store/categories.tsx
@@ -1,47 +1,44 @@
 export interface SuperCat {
   title: string;
-  queryValue: string;
-  category: string;
   subCat: SubCat[];
 }
 
 export interface SubCat {
   title: string;
-  queryValue: string;
-  category: string;
   subCat: SubCat[] | Categories[];
 }
 
 interface Categories {
   title: string;
+  queryValue: string;
 }
 
 export const categories: SuperCat[] = [
   {
     title: 'Apšvietimo sistemos',
-    queryValue: 'Lights',
-    category: 'Apšvietimo sistemos',
     subCat: [
       {
         title: 'Galinis žibintas',
-        queryValue: 'Rear Lights',
-        category: 'Apšvietimo sistemos',
-        subCat: [{ title: 'Atbulinės eigos žibintai' }],
+        subCat: [
+          {
+            title: 'Atbulinės eigos žibintai',
+            queryValue: 'Reverse lights',
+          },
+          {
+            title: 'Galinio žibinto apdaila(juostelė)',
+            queryValue: 'Rear light decoration',
+          },
+        ],
       },
     ],
   },
   {
     title: 'Kėbulo dalys',
-    queryValue: 'Kėbulas',
-    category: 'Kėbulo dalys',
     subCat: [
       {
         title: 'Priekinės kėbulo dalys',
-        queryValue: 'Rear Lights',
-        category: 'Apšvietimo sistemos',
-        subCat: [{ title: 'Kapotas' }],
+        subCat: [{ title: 'Kapotas', queryValue: 'Reverse lights' }],
       },
     ],
   },
 ];
-
