7e739cb96399b0fabd58640f82029189e77519b9
Tadas Balsys <balsys.tadas@gmail.com>
2020-08-11 22:21:55 +0300

    Working solution for nested menu. Menu items are rendered in recursion pattern.

---
+++ src/components/check-box/CheckBox.comp.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './CheckBox.styles.scss';
+
+interface Props {
+  selected: any;
+  label: any;
+  onChange: any;
+}
+
+const CheckBox: React.FC<Props> = ({ selected, label, onChange }) => {
+  console.log(` CheckBox ${label}`);
+  return (
+    <li className='checkbox'>
+      <div
+        className={'checkbox__icon'}
+        onClick={() => onChange(!selected)}
+      ></div>
+      <FontAwesomeIcon icon={'check-square'} />
+      <div className='label'>{label}</div>
+    </li>
+  );
+};
+
+export default CheckBox;
+
+/**
+ 
+  const Checkbox2 = ({ selected, label, onChange }) => {
+  return (
+      <div className="checkbox">
+      <div 
+        className={
+          classNames('fa', 'fa-2x', 'checkbox__icon', selected ? 'fa-check-square' : 'fa-square')} onClick={() => onChange(!selected)}></div>    
+      <div className="checkbox__label">{label}</div>
+    </div>
+  )
+}
+
+
+
+*/
--- src/components/member/member.comp.tsx
+++ src/components/member/member.comp.tsx
@@ -1,22 +1,63 @@
 import React from 'react';
-import { Categories } from '../../store/categories';
+
+import CheckBox from '../check-box/CheckBox.comp';
 
 import './member.styles.scss';
 
 interface Props {
-  category: Categories;
-  // isVisible: boolean;
+  options: any;
+  selectedOptions: any;
+  onChange: any;
 }
 
-const Member: React.FC<Props> = (props: Props) => {
+const Member: React.FC<Props> = ({ options, selectedOptions, onChange }) => {
+  const handleCheckboxClicked = (selectedOptionId: any) => {
+    console.log('handleCheckboxClicked');
+    // is currently selected
+    if (selectedOptions[selectedOptionId]) {
+      // remove selected key from options list
+      delete selectedOptions[selectedOptionId];
+    } else {
+      // is not currently selected
+      // Add selected key to optionsList
+      selectedOptions[selectedOptionId] = {};
+    }
+    // call onChange function given by parent
+    onChange(selectedOptions);
+  };
+
+  const handleSubOptionsListChange = (optionId: any, subSelections: any) => {
+    console.log('handleSubOptionsListChange');
+    // add sub selections to current optionId
+    selectedOptions[optionId] = subSelections;
+    // call onChange function given by parent
+    onChange(selectedOptions);
+  };
+
   return (
-    <>
-      {
-        <h6 >
-          {props.category.title}
-        </h6>
-      }
-    </>
+    <div>
+      {options.map((option: any) => (
+        <ul>
+          <CheckBox
+            selected={selectedOptions[option.id]}
+            label={option.title}
+            onChange={() => {
+              handleCheckboxClicked(option.id);
+            }}
+          />
+          {/* Base Case */}
+          {option.subOptions.length > 0 && selectedOptions[option.id] && (
+            <Member
+              options={option.subOptions}
+              selectedOptions={selectedOptions[option.id]}
+              onChange={(subSelections: any) =>
+                handleSubOptionsListChange(option.id, subSelections)
+              }
+            />
+          )}
+        </ul>
+      ))}
+    </div>
   );
 };
 
