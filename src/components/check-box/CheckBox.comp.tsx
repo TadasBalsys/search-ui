4642815f029d5a0f5392e7f60618f1ff0465e767
Tadas Balsys <balsys.tadas@gmail.com>
2020-08-12 10:43:21 +0300

    Refactoted 'any' types.

--- src/components/check-box/CheckBox.comp.tsx
+++ src/components/check-box/CheckBox.comp.tsx
@@ -1,43 +1,25 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { State as subOptions } from '../side-menu/SideMenu.comp';
 
 import './CheckBox.styles.scss';
 
 interface Props {
-  selected: any;
-  label: any;
-  onChange: any;
+  selected: subOptions;
+  label: string;
+  onChange: (selectedOptionId: boolean) => void;
 }
 
 const CheckBox: React.FC<Props> = ({ selected, label, onChange }) => {
-  console.log(` CheckBox ${label}`);
   return (
     <li className='checkbox'>
       <div
         className={'checkbox__icon'}
         onClick={() => onChange(!selected)}
       ></div>
-      <FontAwesomeIcon icon={'check-square'} />
       <div className='label'>{label}</div>
     </li>
   );
 };
 
 export default CheckBox;
-
-/**
- 
-  const Checkbox2 = ({ selected, label, onChange }) => {
-  return (
-      <div className="checkbox">
-      <div 
-        className={
-          classNames('fa', 'fa-2x', 'checkbox__icon', selected ? 'fa-check-square' : 'fa-square')} onClick={() => onChange(!selected)}></div>    
-      <div className="checkbox__label">{label}</div>
-    </div>
-  )
-}
-
-
-
-*/
